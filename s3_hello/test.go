79d118b77ce2febb9f3d8eac22f00eb49b1adb08
humancool <humancool@gmail.com>
2014-07-01 21:44:44 +0800

    Adding s3_hello file and the testing file

---
+++ s3_hello/test.go
@@ -0,0 +1,32 @@
+// s3 API test.go file, for learning purpose.
+package main
+
+import (
+	"fmt"
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
+)
+
+func main() {
+
+	// Set up s3 connection
+	auth := aws.Auth{
+		AccessKey: "",
+		SecretKey: "",
+	}
+	apnortheast := aws.USEast
+	s3conn := s3.New(auth, apnortheast)
+
+	// connection.Bucket into existing file.
+	fmt.Println("Awesome Eric")
+	mybucket := s3conn.Bucket("humancool")
+
+	response, err := mybucket.List("", "", "", 1000)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range response.Contents {
+		fmt.Println(v.Key)
+	}
+
+}
