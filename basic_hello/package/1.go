2f54c60ef74c3c690dd378c478e2676fe094ad3d
humancool <humancool@gmail.com>
2014-08-12 17:06:04 +0800

    change the package directory to the relative one

--- basic_hello/package/1.go
+++ basic_hello/package/1.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	// 	"github.com/guanjie/erictools/basic_hello/package/erictest"
+	"./erictest"
 )
 
 func main() {
 	fmt.Println("Awesome Eric!")
-	// 	fmt.Println(erictest.Increment(100))
+	fmt.Println(erictest.Increment(35))
 }
