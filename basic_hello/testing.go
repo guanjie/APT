9d8c37b78db07f9330690d0219aa837a930aa917
humancool <humancool@gmail.com>
2014-08-26 13:31:25 +0800

    Adding project Euler 2 and changed names of bunch projects to miniprojects

--- basic_hello/testing.go
+++ basic_hello/testing.go
@@ -35,4 +35,13 @@ func main() {
 	//test5 - Checking the number of chars in the string.
 	fmt.Println("The string char length is: " + strconv.Itoa(len("361a585dc0b297027311b4d0123c55e4")))
 
+	// test6 - test on the slices length.
+	letters := []string{"a", "b", "c", "d"}
+	fmt.Println(len(letters))
+
+	letters = append(letters, "e")
+	fmt.Println(letters)
+	fmt.Println(len(letters))
+	fmt.Println(letters[len(letters)-1])
+
 }
