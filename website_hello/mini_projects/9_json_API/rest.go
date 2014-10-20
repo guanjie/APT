0c907e97f716593cad989184060a727a438d24a1
humancool <humancool@gmail.com>
2014-10-20 13:33:52 +0800

    Adding 9_json_API project and rest.go file for starter

---
+++ website_hello/mini_projects/9_json_API/rest.go
@@ -0,0 +1,51 @@
+// rest.go file for Json file manipulating and learning purpose.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Payload struct {
+	Stuff Data
+}
+type Data struct {
+	Fruit   Fruits
+	Veggies Vegetables
+}
+type Fruits map[string]int
+type Vegetables map[string]int
+
+func serveRest(w http.ResponseWriter, r *http.Request) {
+	response, err := getJsonResponse()
+	if err != nil {
+		log.Fatalf("getJsonResponse -> %v", err)
+	}
+	fmt.Fprintf(w, string(response))
+}
+
+func main() {
+	fmt.Println("Awesome Eric!")
+
+	http.HandleFunc("/", serveRest)
+	http.ListenAndServe(":8080", nil)
+}
+
+func getJsonResponse() ([]byte, error) {
+	fruits := make(map[string]int)
+	fruits["Apple"] = 25
+	fruits["Orange"] = 30
+
+	veggies := make(map[string]int)
+	veggies["Carrots"] = 21
+	veggies["Tomatos"] = 40
+
+	// Struct can directly be transformed into Json type by json.Marshal
+	d := Data{fruits, veggies}
+	p := Payload{d}
+
+	return json.MarshalIndent(p, "", "   ")
+}
