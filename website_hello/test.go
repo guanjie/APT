3df575f061b7f78c251ea8131b337fb18b0746a4
humancool <humancool@gmail.com>
2014-10-20 11:12:20 +0800

    Adding download flv file and put the code into test.go for test3 in the .go file

--- website_hello/test.go
+++ website_hello/test.go
@@ -5,9 +5,10 @@ package main
 import (
 	// 	"fmt"
 	// 	"github.com/codegangsta/martini"
-	// 	"net/http"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
+	// 	"os"
 )
 
 func main() {
@@ -24,8 +25,36 @@ func main() {
 	// 	m.Run()
 
 	// test2 - Check os.MkdirAll
-	err := os.MkdirAll("/Users/guanjiehe/Desktop/try", 0777)
+	// 	err := os.MkdirAll("/Users/guanjiehe/Desktop/try", 0777)
+	// 	if err != nil {
+	// 		log.Fatalf("os.MkdirAll -> %v", err)
+	// 	}
+
+	// test3 - actually download the flv file...Be aware this is porn XXX
+	var url = "http://im.c6881952.c59b2a8.cdn2c.videos2.youjizz.com/c/1/c11fa9963ac4218933ea87c8091b71c21413528902-640-480-1800-h264.flv?rs=350&ri=1400&s=1413687934&e=1413860734&h=0c048d8afbe5c431a036393c61d21765"
+	download_to_desktop(url)
+}
+
+func download_to_desktop(url string) {
+	// Get response from the jpg link
+	resp, err := http.Get(url)
+	if resp.StatusCode == 404 {
+		log.Fatal("404 code error")
+		return
+	}
 	if err != nil {
-		log.Fatalf("os.MkdirAll -> %v", err)
+		log.Fatalf("http.Get -> %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Get the data by using ioutil.ReadAll(resp.Body)
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("ioutil.ReadAll -> %v", err)
+	}
+
+	// XXX hardcoded path XXX
+	if err := ioutil.WriteFile("/Users/guanjiehe/Desktop/checkit.flv", data, 0766); err != nil {
+		log.Fatalf("ioutil.WriteFile -> %v", err)
 	}
 }
