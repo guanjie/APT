920d120718000543652f9893e83f1a067b92641e
humancool <humancool@gmail.com>
2014-10-27 17:33:15 +0800

    Adding test 2 code for hello.go file

--- mongodb_hello/hello.go
+++ mongodb_hello/hello.go
@@ -3,25 +3,28 @@ package main
 
 import (
 	"fmt"
+	// 	"github.com/kr/pretty"
 	"labix.org/v2/mgo"
-	// 	"labix.org/v2/mgo/bson"
+	"labix.org/v2/mgo/bson"
 	// 	"strconv"
+	"log"
 	"time"
 )
 
 type (
+	// We can do omitempty
 	Todo struct {
-		Task      string    `bson:"task"`
-		Created   time.Time `bson:"created"`
-		Updated   time.Time `bson:"updated"`
-		Completed time.Time `bson:"completed"`
+		Task      string    `bson:"task,omitempty"`
+		Created   time.Time `bson:"created,omitempty"`
+		Updated   time.Time `bson:"updated,omitempty"`
+		Completed time.Time `bson:"completed,omitempty"`
 	}
 )
 
 func main() {
 	// declare 2 todos
-	todo1 := Todo{Task: "1st test", Created: time.Now()}
-	todo2 := Todo{Task: "2nd test", Created: time.Now()}
+	// 	todo1 := Todo{Task: "1st test", Created: time.Now()}
+	// 	todo2 := Todo{Task: "2nd test", Created: time.Now()}
 
 	// Open mongoSession
 	fmt.Println("Awesome Eric!")
@@ -32,6 +35,29 @@ func main() {
 	defer mongoSession.Close()
 
 	collection := mongoSession.DB("advent").C("wishes")
-	collection.Insert(todo1)
-	collection.Insert(todo2)
+	// 	collection.Insert(todo1)
+	// 	collection.Insert(todo2)
+
+	// Test 1: Adding a [string]interface{} map
+	// We can also insert the map data that we pre-assumbled.
+	// 	m := map[string]interface{}{
+	// 		"name": "godep",
+	// 		"tags": []string{"tool", "dependency"},
+	// 		"contact": bson.M{
+	// 			"name":  "Guanjie He",
+	// 			"email": "humancool@gmail.com",
+	// 		},
+	// 	}
+	// 	err = collection.Insert(m)
+	if err != nil {
+		log.Fatalf("collection insert -> %v", err)
+	}
+	fmt.Println("Okay!")
+
+	// Test 2: Changing the information of a colleciton that I already got.
+	// Set the additional data using "$set" inside the new M map[string]interface{}, tried Upsert, Update and UpdateAll
+	type M map[string]interface{}
+	change := M{"$set": Todo{Updated: time.Now()}}
+	collection.UpdateAll(bson.M{"nbbbb": "nbme"}, change)
+
 }
