d204b9d38f9f181a83628213790ac9708564120d
humancool <humancool@gmail.com>
2015-03-31 09:03:26 +0800

    Initial Commit

---
+++ basic_tools/iOS_hello/Hop Hero/Hop Hero/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hop Hero
+//
+//  Created by humancool on 3/31/15.
+//  Copyright (c) 2015 humancool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ basic_tools/iOS_hello/Hop Hero/Hop Hero/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Hop Hero
+//
+
+//  Copyright (c) 2015 humancool. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
